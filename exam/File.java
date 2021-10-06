d3c214a5201a1c371beccdb79342e0bcf442bf9b
AnittaShaijy <84674627+AnittaShaijy@users.noreply.github.com>
2021-10-06 12:39:20 +0530

    Add files via upload

---
+++ exam/File.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class File
+{
+ public static void main(String args[]) throws IOException, FileNotFoundException
+ {
+ int i;
+ File f1=("D:Home//Documents//f1");
+ File f2=("D:Home//Documents//f2");
+ File InputStream=new FileInputStream(f1);
+ File OutputStream=new FileOutputStrem(f2);
+ do{
+   in.read(f1);
+   if(i!=-1)
+    in.write(f1);
+    {
+    system.out.println("This is a File");
+    }
+    op.cpy(f2);
+    }while(i=-1);
+    {
+    op.read();
+    op.write();
+    op.cpy();
+    in.close();
+    op.close();
+    }
+    }
+    }
+    
+   
---
+++ exam/Reverse.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+class Reverse
+{
+public static void main(String args[])
+{
+ int num;
+ int rev;
+ Reverse r=new Reverse();
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter the number:" + num);
+ num=NextInt();
+ 
+ rev=(num+rev)*rev;
+ num=num/10;
+ {
+System.out.println(Reverse of the number is:" " +rev);
+ }
+ }
+ }
+ 
